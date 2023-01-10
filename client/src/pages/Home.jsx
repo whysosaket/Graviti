3c09968892c87dea26c29d3cfadcf1068e0cfdf1
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-01-11 05:05:09 +0530

    Made Website Responsive

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import './Home.css'
+import './HomeR.css'
 import Input from '../components/Input'
 
 const Home = () => {
@@ -8,10 +9,16 @@ const Home = () => {
     <div>
         <Navbar />
         <div className='half'>
-        <h5 className='text-center py-5'>Let's calculate <strong>distance</strong> from Google map</h5>
+        <h5 className='title text-center py-5'>Let's calculate <strong>distance</strong> from Google map</h5>
+
+
+        <div className='right float-right w-50 d-flex'>
+          <img className='map mx-auto' src='https://akhromieiev.com/travel/how-to-find-most-visiting-places-in-city-using-google-maps/img/wp-content-uploads-2017-09-AmsterdamMap-1024x1024.png' alt='map'/>
+        </div>
+
         <div className='left float-left'>
-            <div className='lefttop w-100 d-flex'>
-            <div className='leftleft w-50 d-inline-block'>
+            <div className='lefttop w-100 d-md-flex'>
+            <div className='leftleft w-50'>
             <Input label="Origin" inputText="Select an Origin"/>
             <div className='mb-4'>
                 <Input label="Stop" inputText="Select a Stop"/>
@@ -31,27 +38,27 @@ const Home = () => {
 
             <div className='leftbottom w-100'>
             <div className="jumbotron border">
+
               <div className='jum-top d-flex'>
-                <div className='jum-top-left align-middle my-auto w-50 d-inline-block'>
+              
+              <div className='jum-top-left align-middle my-auto w-50 d-inline-block'>
               <h4 className='text-center'><strong>Distance</strong></h4>
               </div>
+
               <div className='jum-top-right align-middle my-auto  w-50 d-inline-block'>
                 <h2 className='text-center'><strong>1,427 kms</strong></h2>
-                </div>
+              </div>
+
               </div>
 
               <div className='jum-bottom'>
-                <div className='container d-flex'>
+                <div className='container d-md-flex'>
                   <p className='my-3'>The distance between <strong>Mumbai</strong> and <strong>Delhi</strong> via the seleted route is <strong>1,427 kms</strong>.</p>
                 </div>
               </div>
  
             </div>
             </div>
-        </div>
-
-        <div className='right float-right w-50 d-flex'>
-          <img className='map mx-auto' src='https://akhromieiev.com/travel/how-to-find-most-visiting-places-in-city-using-google-maps/img/wp-content-uploads-2017-09-AmsterdamMap-1024x1024.png' alt='map'/>
         </div>
 
 
